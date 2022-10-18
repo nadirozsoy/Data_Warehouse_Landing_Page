ec64a9986aab7c5456cc40ed271a508b240c0838
Nadir Özsoy <thisisnadirozsoy@gmail.com>
2022-10-18 21:03:55 +0300

    Master 4 | section v3

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// const menu = document.querySelector('.head__nav__menu')
+// const ul = document.querySelector('.head__nav__ul')
+
+// menu.addEventListener('click', () => ul.classList.toggle('mobile'))
+//* ---------------------------
+// const menu = document.querySelector('.head__nav__menu')
+
+// menu.addEventListener('click', mobileNav)
+
+// function mobileNav() {
+// const ul = document.querySelector('.head__nav__ul')
+// ul.forEach((b) => {
+// if (b.target.className === 'head__nav__menu') {
+// ul.classList.toggle('mobile')
+// }
+// })
+// }
--- src/app.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.head__nav__menu')
-const ul = document.querySelector('.head__nav__ul')
-
-menu.addEventListener('click', () => {
-  ul.classList.toggle('mobile')
-})
