30fcb0281790c2f13a2e85ce0c04a54e01f86293
Nadir Özsoy <thisisnadirozsoy@gmail.com>
2022-10-21 19:06:27 +0300

    Master 5 | footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,13 @@
 // }
 // })
 // }
+
+let swiper = new Swiper('.mySwiper', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
